4928154c9f4ff6f049a8e2c431a7d7827b7b0041
lml <2084387897@qq.com>
2022-09-29 18:32:05 +0800

    feat(custom): 基础配置

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    214332432
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>213 2 12 54 df df 3 14</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      a: '123',
+      v: '12'
+    }
   }
 }
 </script>
